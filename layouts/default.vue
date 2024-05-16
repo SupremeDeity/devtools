65f7cb8b7d91ae9784237b469808511ff1b76950
SupremeDeity <29257166+SupremeDeity@users.noreply.github.com>
2024-05-16 21:53:20 +0500

    Add link to scheduler page on index page.

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,10 +2,10 @@
   <div class="font-['Urbanist']">
     <div
       class="fixed w-full z-50 shadow shadow-gray-200 dark:shadow-gray-800 flex items-center justify-between bg-gray-100 dark:bg-gray-900 text-gray-500 dark:text-gray-400 p-3 ">
-      <div class="flex items-center gap-x-2 text-2xl select-none">
+      <NuxtLink href="/" class="flex items-center gap-x-2 text-2xl select-none">
         <Icon class="text-primary-500 dark:text-primary-400" name="bi:tools" /> <span
           class="font-bold text-gray-900 dark:text-gray-100">DevTools</span>
-      </div>
+      </NuxtLink>
 
       <ThemeModeToggle />
     </div>
--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,25 @@
 <template>
     <div class="p-6">
-        <UAlert icon="i-heroicons-information-circle"
+        <UAlert icon="i-heroicons-bell-alert"
             description="This site is under development and you might find bugs and uncompleted things. Open up a issue on github if you find a bug."
             title="Heads up!" variant="subtle" color="primary">
             <template #title="{ title }">
                 <!-- eslint-disable-next-line vue/no-v-html -->
-                <span v-html="title" />
+                <span class="font-bold" v-html="title" />
+            </template>
+            <template #icon="{ icon }">
+                <UBadge size="md">
+                    <UIcon :name="icon" />
+                </UBadge>
             </template>
         </UAlert>
+        <NuxtLink class="block w-min" href="/scheduler-os">
+            <UCard
+                class="w-max mt-4 !bg-primary-900 border !border-primary-400 hover:!bg-primary-950 hover:!border-primary-600 transition-colors">
+                <div class="size-32 flex items-center justify-center font-bold text-lg text-center">
+                    Process Scheduler
+                </div>
+            </UCard>
+        </NuxtLink>
     </div>
 </template>
